ecfa23cf2c5a4f30d3d9bd0ce6d93dc99003f0d3
BAC <balaianggunceria@gmail.com>
2026-01-15 10:00:49 +0700

    updateapiapi2

--- src/api/api.js
+++ src/api/api.js
@@ -3,9 +3,9 @@ const local = 'http://localhost:5000'
 const production = 'https://bac-backend-web.onrender.com'
 
 let api_url = ''
-let MODE = 'pro'
+let mode = 'pro'
 
-if (MODE === 'pro') {
+if (mode === 'pro') {
     api_url = production
 }else{
     api_url = local
